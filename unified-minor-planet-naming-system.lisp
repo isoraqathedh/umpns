bea096094480268b6dda51644b6bc3d7ed6251f7
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2015-08-10 21:22:41 +0800

    Enter the database.
    (Database is empty right now.)

--- unified-minor-planet-naming-system.lisp
+++ unified-minor-planet-naming-system.lisp
@@ -6,6 +6,10 @@
    :umpns))
 (in-package :umpns)
 
+;; Connect to the database
+(defvar *db* (connect (uiop:native-namestring (asdf:system-relative-pathname :umpns "mpcorb.db"))))
+
+;; Some parsing stuff
 (defun packed-form-p (string)
   "Checks if a string is a packed form or not."
   (cl-ppcre:scan "^[IJK][0-9][0-9][A-HJ-Y][A-Za-z0-9][0-9][A-HJ-Z]$" string))
@@ -34,3 +38,15 @@
       (position
        (char packed-form 4)
        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlmnopqrstuvwxyz"))))
+
+(defun parse-packed-form (packed-form)
+  "Turns a packed-form string into a list of two things: 
+half-month of discovery and serial number."
+  (list :discovery-half-month
+        (let ((half-month (packed-form-half-month packed-form)))
+          (encode-universal-time
+           0 0 0 ; These values are irrelevant
+           (if (evenp half-month) 1 15)
+           (1+ (floor (/ half-month 2)))
+           (packed-form-year packed-form)))
+        :serial-number (packed-form-serial-number packed-form)))
